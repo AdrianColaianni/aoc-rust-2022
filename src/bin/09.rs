cb356dba7a27a0b679107baf103cd330cd7993cd
Adrian Colaianni <adrian@colaianni.us>
2023-05-22 18:05:13 -0400

    Finished day 9

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,262 +1,268 @@
-#[derive(Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
-struct Position {
-    y: i32,
-    x: i32,
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::{cmp::Ordering::*, ops::{Add, AddAssign}, fmt::Debug};
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Point {
+    x: isize,
+    y: isize,
 }
 
-impl core::fmt::Debug for Position {
+impl Debug for Point {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
 
-#[derive(Debug)]
-enum Dir {
-    U,
-    D,
-    L,
-    R,
-}
-
-#[derive(Debug, Clone)]
-struct Tail {
-    pos: Position,
-    visited: Vec<Position>,
-}
-
-impl Tail {
-    pub fn new() -> Self {
-        let pos = Position { x: 0, y: 0 };
-        Tail {
-            pos,
-            visited: vec![pos],
-        }
-    }
-    pub fn visited(&self) -> u32 {
-        self.visited.len() as u32
+impl Point {
+    pub fn new(x: isize, y: isize) -> Self {
+        Point { x, y }
     }
 
-    pub fn move_tail(&mut self, x: i32, y: i32) {
-        self.pos.x += x;
-        self.pos.y += y;
+    pub fn too_far(&self, other: &Point) -> bool {
+        self.x.abs_diff(other.x) > 1 || self.y.abs_diff(other.y) > 1
+    }
+}
 
-        if self.visited.binary_search(&self.pos).is_err() {
-            let idx = self.visited.partition_point(|&x| x < self.pos);
-            self.visited.insert(idx, self.pos);
-            // println!("New visited: {:?}", self.visited);
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
         }
     }
 }
 
-#[derive(Debug)]
+impl AddAssign for Point {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+    }
+}
+
+#[derive(Clone)]
+struct Knot {
+    p: Point,
+    past_pos: Vec<Point>,
+}
+
+impl Debug for Knot {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{:?}: {:?}", self.p, self.past_pos)
+    }
+}
+
+impl Knot {
+    pub fn new(x: isize, y: isize) -> Self {
+        let p = Point::new(0, 0);
+        Self {
+            p,
+            past_pos: vec![p],
+        }
+    }
+
+    pub fn add_pos(&mut self, pos: Point) {
+        self.p += pos;
+
+        // println!("New pos: ({}, {})", self.p.x, self.p.y);
+
+        // Insert in order if not found
+        if self.past_pos.binary_search(&pos).is_err() {
+            let idx = self.past_pos.partition_point(|&x| x < pos);
+            self.past_pos.insert(idx, pos);
+        }
+    }
+
+    pub fn move_near_pos(&mut self, pos: &Point) {
+        let x = pos.x - self.p.x;
+        let y = pos.y - self.p.y;
+
+        let adjust = |z: isize| -> isize {
+            if z.is_positive() {
+                -1
+            } else {
+                1
+            }
+        };
+
+        // print!("from {:?} ", self.p);
+
+        if x == 0 {
+            // print!("x: y={y} ");
+            self.p.y = pos.y + adjust(y);
+        } else if y == 0 {
+            // print!("y: x={x} ");
+            self.p.x = pos.x + adjust(x);
+        } else {
+            // print!("else ({}, {}) ", x, y);
+            match x.abs().cmp(&y.abs()) {
+                Less => {
+                    // x < y
+                    self.p.x = pos.x;
+                    self.p.y = pos.y + adjust(y);
+                }
+                Equal => {
+                    self.p.x = pos.x + adjust(x);
+                    self.p.y = pos.y + adjust(y);
+                }
+                Greater => {
+                    // x > y
+                    self.p.x = pos.x + adjust(x);
+                    self.p.y = pos.y;
+                }
+            }
+        }
+
+        // println!("to {:?}", self.p);
+
+        if self.past_pos.binary_search(&self.p).is_err() {
+            let idx = self.past_pos.partition_point(|&x| x < self.p);
+            self.past_pos.insert(idx, self.p);
+        }
+    }
+
+    pub fn visits(&self) -> usize {
+        self.past_pos.len()
+    }
+}
+
 struct Rope {
-    head: Position,
-    tails: Vec<Tail>,
-    max: Position,
-    min: Position,
+    rope: Vec<Knot>,
+    min: Point,
+    max: Point,
 }
 
 impl Rope {
-    pub fn new(t_count: usize) -> Self {
-        let pos = Position { x: 0, y: 0 };
+    pub fn new(len: usize) -> Self {
+        let knot = Knot::new(0, 0);
+        let p = Point::new(0, 0);
         Rope {
-            head: pos,
-            tails: vec![Tail::new(); t_count],
-            max: pos,
-            min: pos,
+            rope: vec![knot; len],
+            min: p,
+            max: p,
         }
     }
 
-    pub fn tail_too_far(&self, i: usize) -> bool {
-        if i >= self.tails.len() {
-            panic!("Attemped to find the distance of non-existant tail");
-        }
+    pub fn move_rope(&mut self, motion: &str) {
+        let (dir, dist) = Self::parse_move(motion);
 
-        let tail = &self.tails[i].pos;
-        let prev = if i == 0 {
-            &self.head
-        } else {
-            &self.tails[i - 1].pos
+        // println!("Moving {:?}x{}", dir, dist);
+
+        for _ in 0..dist {
+            self.rope[0].add_pos(dir);
+            self.update_bounds();
+            self.move_tail();
+            // self.print_board();
+        }
+    }
+
+    fn parse_move(motion: &str) -> (Point, usize) {
+        let motion: Vec<&str> = motion.split(' ').collect();
+        let dir = match motion[0] {
+            "U" => Point::new(0, 1),
+            "D" => Point::new(0, -1),
+            "L" => Point::new(-1, 0),
+            "R" => Point::new(1, 0),
+            _ => panic!("Impossible motion given"),
         };
+        let dist: usize = motion[1].parse().expect("Unparsable distance given");
 
-        prev.x.abs_diff(tail.x) > 1 || prev.y.abs_diff(tail.y) > 1
+        (dir, dist)
     }
 
-    pub fn move_head(&mut self, d: &Dir) {
-        match d {
-            Dir::U => self.head.y += 1,
-            Dir::D => self.head.y -= 1,
-            Dir::L => self.head.x -= 1,
-            Dir::R => self.head.x += 1,
-        }
-        // println!(" {:?}", self.head);
+    fn move_tail(&mut self) {
+        for i in 1..self.rope.len() {
+            let lead = &self.rope[i - 1];
+            let lead = lead.p;
+            let cur = &mut self.rope[i];
 
-        self.update_range();
-
-        for i in 0..self.tails.len() {
-            if !self.move_tail(i) {
+            if !cur.p.too_far(&lead) {
                 break;
             }
-        }
 
-        // self.print_board();
-    }
+            // print!("Moving tail [{i}]: ");
 
-    fn update_range(&mut self) {
-        if self.max.x < self.head.x {
-            self.max.x = self.head.x;
-        }
-        if self.max.y < self.head.y {
-            self.max.y = self.head.y;
-        }
-        if self.min.x > self.head.x {
-            self.min.x = self.head.x;
-        }
-        if self.min.y > self.head.y {
-            self.min.y = self.head.y;
+            cur.move_near_pos(&lead);
         }
     }
 
-    pub fn tail_visits(&mut self, i: usize) -> u32 {
-        self.tails[i].visited()
+    pub fn visits(&self, i: usize) -> usize {
+        self.rope[i].visits()
     }
 
-    fn move_tail(&mut self, i: usize) -> bool {
-        if !self.tail_too_far(i) {
-            return false;
+    fn update_bounds(&mut self) {
+        let head = &self.rope[0].p;
+        if head.x > self.max.x {
+            self.max.x = head.x
         }
-        let tail = self.tails[i].to_owned().pos;
-        let front = if i == 0 {
-            &self.head
-        } else {
-            &self.tails[i - 1].pos
-        };
-
-        let x = front.x - tail.x;
-        let y = front.y - tail.y;
-        let (x, y) = match (x, x.is_positive(), y, y.is_positive()) {
-            (x, _, 0, _) => ((x - 1).max(-1), 0),
-            (0, _, y, _) => (0, (y - 1).max(-1)),
-            (x, true, -1, _) => (x - 1, -1),
-            (x, false, -1, _) => (x + 1, -1),
-            (-1, _, y, true) => (-1, y - 1),
-            (-1, _, y, false) => (-1, y + 1),
-            (x, true, 1, _) => (x - 1, 1),
-            (x, false, 1, _) => (x + 1, 1),
-            (x, _, 2, _) => (x, 1),
-            (x, _, -2, _) => (x, -1),
-            (2, _, y, _) => (1, y),
-            (-2, _, y, _) => (-1, y),
-            (x, true, y, true) => match x.cmp(&y) {
-                std::cmp::Ordering::Less => {
-                    self.print_board();
-                    todo!("Unimplemented move ({}, {})", x, y);
-                }
-                std::cmp::Ordering::Equal => (x / 2, y / 2),
-                std::cmp::Ordering::Greater => {
-                    self.print_board();
-                    todo!("Unimplemented move ({}, {})", x, y);
-                }
-            },
-            (x, _, y, _) => {
-                self.print_board();
-                panic!("Reached unforscene move_tail position ({x}, {y})")
-            }
-        };
-
-        self.tails[i].move_tail(x, y);
-
-        // println!(
-        //     "Tail {} is too far!  Moving from {:?} to {:?}",
-        //     i, tail, self.tails[i].pos
-        // );
-
-        true
+        if head.x < self.min.x {
+            self.min.x = head.x
+        }
+        if head.y > self.max.y {
+            self.max.y = head.y
+        }
+        if head.y < self.min.y {
+            self.min.y = head.y
+        }
     }
 
     pub fn print_board(&self) {
-        println!(
-            "Head: {:?}, min: {:?}, max: {:?}",
-            self.head, self.min, self.max
-        );
-        for y in (self.min.y..=self.max.y).rev() {
-            'x: for x in self.min.x..=self.max.x {
-                let pos = Position { x, y };
-                if self.head == pos {
-                    print!("H");
-                    continue 'x;
+        let width = self.max.x - self.min.x;
+        let heigh = self.max.y - self.min.y;
+
+        for _ in 0..width {
+            print!("-");
+        }
+
+        println!("\n{:?}", self.rope);
+
+        for y in (0..=heigh).rev() {
+            'w: for x in 0..=width {
+                if x == 0 && y == 0 {
+                    print!("s");
+                    continue 'w;
                 }
 
-                for (i, tail) in self.tails.iter().enumerate() {
-                    if tail.pos == pos {
-                        print!("{}", i + 1);
-                        continue 'x;
+                let p = Point::new(x, y);
+                for i in 0..self.rope.len() {
+                    if self.rope[i].p == p {
+                        print!("{i}");
+                        continue 'w;
                     }
                 }
 
-                if x == 0 && y == 0 {
-                    print!("s");
-                } else {
-                    print!(",");
-                }
+                print!(",");
             }
             println!();
         }
+        for _ in 0..width {
+            print!("-");
+        }
+        println!();
     }
-}
-
-fn read_input(input: &str) -> Vec<(Dir, i32)> {
-    input
-        .lines()
-        .map(|x| {
-            // println!("Reading line {}", x);
-            let line: Vec<&str> = x.split(' ').collect();
-            let dir = match line[0] {
-                "U" => Dir::U,
-                "D" => Dir::D,
-                "L" => Dir::L,
-                "R" => Dir::R,
-                _ => panic!("Read faulty direction"),
-            };
-            let dst: i32 = line[1].parse().unwrap();
-
-            (dir, dst)
-        })
-        .collect()
 }
 
 pub fn part_one(input: &str) -> Option<i32> {
-    let input: Vec<(Dir, i32)> = read_input(input);
-    let mut rope = Rope::new(1);
+    let mut rope = Rope::new(2);
 
-    // println!("{:?}", input);
+    input.lines().for_each(|x| {
+        // println!("Line {}", x);
+        rope.move_rope(x);
+    });
 
-    for inst in input {
-        for _ in 0..inst.1 {
-            // print!("Moved head {:?}", inst.0);
-            rope.move_head(&inst.0);
-        }
-    }
-
-    Some(rope.tail_visits(0) as i32)
+    Some(rope.visits(1) as i32)
 }
 
-// > 2650
 pub fn part_two(input: &str) -> Option<i32> {
-    let input: Vec<(Dir, i32)> = read_input(input);
-    let mut rope = Rope::new(9);
+    // return None;
+    let mut rope = Rope::new(10);
 
-    // println!("{:?}", input);
+    input.lines().for_each(|x| rope.move_rope(x));
 
-    for inst in input {
-        for _ in 0..inst.1 {
-            // print!("Moved head {:?}", inst.0);
-            rope.move_head(&inst.0);
-        }
-        // rope.print_board();
-    }
-
-    Some(rope.tail_visits(8) as i32)
+    Some(rope.visits(9) as i32)
 }
 
 fn main() {
